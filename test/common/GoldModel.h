fa56ea15ddef4c9a5d37d429f779e9264bda17a7
Kartik Prabhu <kprabhu7@stanford.edu>
2022-02-22 12:25:38 -0800

    Add no-norm to gold model
    Former-commit-id: be2522dd014e581a5add362471f583782ac6f7f4

--- test/common/GoldModel.h
+++ test/common/GoldModel.h
@@ -111,7 +111,20 @@ void run_gold_op(const SimplifiedParams params, T *matrixA, T *matrixB,
       matrixC[k] = acc;
     }
   } else if (params.NO_NORM) {
-    // not yet implemented
+    // elementwise multiplication and addition of matrices
+    int C = params.loops[1][params.reductionLoopIndex[1]] * DIMENSION;
+    int K = params.loops[0][params.weightLoopIndex[0]] *
+            params.loops[1][params.weightLoopIndex[1]] * DIMENSION;
+    for (int k = 0; k < K; k++) {
+      for (int c = 0; c < C; c++) {
+        ACC_T a = matrixA[c * K + k];
+        ACC_T b = matrixB[c * K + k];
+        ACC_T bias = biasMatrix[c * K + k];
+        ACC_T acc = gold_fma(a, b, bias);
+
+        matrixC[c * K + k] = acc;
+      }
+    }
   } else {  // normal operation
 
     int X = params.loops[0][params.inputXLoopIndex[0]] *
