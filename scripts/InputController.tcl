86211ce856ef403aef7fdfa4a67f3fe81ce33c72
Kartik Prabhu <kprabhu7@stanford.edu>
2022-07-25 16:52:23 -0700

    Map transpose buffer to registers
    Former-commit-id: 3d1b40439559e018aefa33eb161b36406d1b560d

--- scripts/InputController.tcl
+++ scripts/InputController.tcl
@@ -3,6 +3,7 @@ source scripts/architecture.tcl
 
 set block "InputController"
 set full_block_name "InputController<$IO_DATATYPE, $DIMENSION>"
+set input_controller_stripped [string map {" " ""} $full_block_name]
 
 source scripts/common.tcl
 
@@ -11,5 +12,7 @@ go libraries
 directive set -CLOCKS $clocks
 
 go assembly
+
+directive set /$input_controller_stripped/$input_controller_stripped:transposer/transposer/while:if:transposeBuffer.bits:rsc -MAP_TO_MODULE {[Register]}
 
 go extract
