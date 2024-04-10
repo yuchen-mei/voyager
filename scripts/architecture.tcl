29627aacef853673c406cdb4215528f03b29c045
Kartik Prabhu <kprabhu7@stanford.edu>
2024-04-09 19:08:44 -0700

    Add RTL generation support for E4M3 using DesignWare
    Former-commit-id: c9dfe2bbef07ba14eee7c63b358e1b01f9021c7c

--- scripts/architecture.tcl
+++ scripts/architecture.tcl
@@ -21,6 +21,17 @@ if { $DATATYPE == "P8_1" } {
 
   set IO_DATATYPE_WIDTH 8
   set ACCUM_DATATYPE_WIDTH 16
+} elseif { $DATATYPE == "E4M3_DW" } {
+  set IO_DATATYPE "F8"
+  set ACCUM_DATATYPE "F16"
+  set INTERMEDIATE_DATATYPE "F16"
+  set PE_INPUT_DATATYPE "StdFloat<3, 4, true, true, AC_RND_CONV>::AccumulationDatatype"
+  set PE_WEIGHT_DATATYPE "StdFloat<3, 4, true, true, AC_RND_CONV>::AccumulationDatatype"
+  set PE_PSUM_DATATYPE "StdFloat<7, 8, true, true, AC_RND_CONV>::AccumulationDatatype"
+  set C_DATA_REP_NAME "float_val.d"
+
+  set IO_DATATYPE_WIDTH 8
+  set ACCUM_DATATYPE_WIDTH 16
 } elseif { $DATATYPE == "E5M2" } {
   set IO_DATATYPE "F8"
   set ACCUM_DATATYPE "F16"
