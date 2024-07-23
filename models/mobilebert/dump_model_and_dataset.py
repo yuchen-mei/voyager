c5d7e3f87d1e6c0b2545fe354c64443600479164
Jeffrey Yu <jeffreyy@stanford.edu>
2024-07-23 13:58:13 -0700

    Add mobilebert-tiny squad model. Implement squad data dumping.
    Former-commit-id: 5b5a95f7bc17b82cb86798bc29db26b333a59a15

--- models/mobilebert/dump_model_and_dataset.py
+++ models/mobilebert/dump_model_and_dataset.py
@@ -1,62 +1,165 @@
-#!/usr/bin/env python3
 import argparse
 import os
-from os import listdir
-import struct
-import sys
-import re
 
-import dill as pickle
 import numpy as np
 import torch
+import torch.nn.functional as F
 
 from datasets import load_dataset, load_metric
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 from transformers import (
     AutoModelForSequenceClassification,
+    AutoModelForQuestionAnswering,
     AutoTokenizer,
     get_scheduler,
     default_data_collator
 )
+from utils import write_fp64
 
-''' @brief: Writes data of form torch.tensor dtype=float64 to binary data. '''
-def write_fp64(filename, data):
-    data = data.astype(np.float64)
-    with open(filename, 'wb') as f:
-        floatlist = []
-        
-        for i in np.nditer(data):
-            floatlist.append(i)
+task_to_keys = {
+    "cola": ("sentence", None),
+    "mnli": ("premise", "hypothesis"),
+    "mrpc": ("sentence1", "sentence2"),
+    "qnli": ("question", "sentence"),
+    "qqp": ("question1", "question2"),
+    "rte": ("sentence1", "sentence2"),
+    "sst2": ("sentence", None),
+    "stsb": ("sentence1", "sentence2"),
+    "wnli": ("sentence1", "sentence2"),
+}
 
-        buf = struct.pack('%sd' % len(floatlist), *floatlist)
-        f.write(buf)
 
 def write_model_params(model, data_dir):
-    parameters = {n: p.data.detach().float().cpu().numpy().T for n, p in model.named_parameters() if p.requires_grad}
-    parameters["classifier.weight"] = np.concatenate([parameters["classifier.weight"].T, np.zeros((14, 512))])
-    parameters["classifier.bias"] = np.concatenate([parameters["classifier.bias"], np.full(14, np.finfo(np.float32).min)])
-
-    if not os.path.exists(os.path.join(data_dir, "params")):
-        os.makedirs(os.path.join(data_dir, "params"))
-
-    for name, param in parameters.items():
+    os.makedirs(os.path.join(data_dir, "params"), exist_ok=True)
+    for name, param in model.named_parameters():
+        weight = param.data.detach().float().cpu()
+        if name in ["classifier.weight", "qa_outputs.weight"]:
+            # stick to the name classifier so that we don't need to change the mapping
+            name = "classifier.weight"
+            weight = F.pad(weight, (0, 0, 0, 14))
+        elif name in ["classifier.bias", "qa_outputs.bias"]:
+            name = "classifier.bias"
+            weight = F.pad(weight, (0, 14), value=float('-inf'))
+        elif weight.dim() == 2:
+            weight = weight.T
         outfile = os.path.join(data_dir, "params", name.replace('.', '_'))
-        write_fp64(outfile, param.flatten())
+        write_fp64(outfile, weight.numpy().flatten())
         print(outfile, end="\r", flush=True)
     print("=" * 120)
 
-def write_dataset(model, glue_dataset, model_dir, data_dir, finetuning=False):
-    'NOTE: only supports SST-2 for now'
 
-    raw_datasets = load_dataset("glue", glue_dataset)
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
+def process_qa_dataset(model, model_name_or_path, dataset, data_dir):
+    max_seq_length = 128
+    doc_stride = 0
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+    raw_datasets = load_dataset(dataset)
+
+    column_names = raw_datasets["train"].column_names
+
+    question_column_name = "question" if "question" in column_names else column_names[0]
+    context_column_name = "context" if "context" in column_names else column_names[1]
+    answer_column_name = "answers" if "answers" in column_names else column_names[2]
+
+    # Padding side determines if we do (question|context) or (context|question).
+    pad_on_right = tokenizer.padding_side == "right"
+
+    max_seq_length = min(max_seq_length, tokenizer.model_max_length)
+
+    # Validation preprocessing
+    def prepare_validation_features(examples):
+        # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+        # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+        # left whitespace
+        examples[question_column_name] = [q.lstrip() for q in examples[question_column_name]]
+
+        # Tokenize our examples with truncation and maybe padding, but keep the overflows using a stride. This results
+        # in one example possible giving several features when a context is long, each of those features having a
+        # context that overlaps a bit the context of the previous feature.
+        tokenized_examples = tokenizer(
+            examples[question_column_name if pad_on_right else context_column_name],
+            examples[context_column_name if pad_on_right else question_column_name],
+            truncation="only_second" if pad_on_right else "only_first",
+            max_length=max_seq_length,
+            stride=doc_stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length",
+        )
+
+        # Since one example might give us several features if it has a long context, we need a map from a feature to
+        # its corresponding example. This key gives us just that.
+        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+
+        # For evaluation, we will need to convert our predictions to substrings of the context, so we keep the
+        # corresponding example_id and we will store the offset mappings.
+        tokenized_examples["example_id"] = []
+
+        for i in range(len(tokenized_examples["input_ids"])):
+            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+            sequence_ids = tokenized_examples.sequence_ids(i)
+            context_index = 1 if pad_on_right else 0
+
+            # One example can give several spans, this is the index of the example containing this span of text.
+            sample_index = sample_mapping[i]
+            tokenized_examples["example_id"].append(examples["id"][sample_index])
+
+            # Set to None the offset_mapping that are not part of the context so it's easy to determine if a token
+            # position is part of the context or not.
+            tokenized_examples["offset_mapping"][i] = [
+                (o if sequence_ids[k] == context_index else None)
+                for k, o in enumerate(tokenized_examples["offset_mapping"][i])
+            ]
+
+        return tokenized_examples
+
+    if "validation" not in raw_datasets:
+        raise ValueError("--do_eval requires a validation dataset")
+    eval_examples = raw_datasets["validation"]
+    # Validation Feature Creation
+    eval_dataset = eval_examples.map(
+        prepare_validation_features,
+        batched=True,
+        num_proc=1,
+        remove_columns=column_names,
+        desc="Running tokenizer on validation dataset",
+    )
+
+    eval_dataset_for_model = eval_dataset.remove_columns(["example_id", "offset_mapping"])
+    eval_dataloader = DataLoader(
+        eval_dataset_for_model, collate_fn=default_data_collator, batch_size=1
+    )
+
+    model.eval()
+
+    for step, batch in enumerate(tqdm(eval_dataloader)):
+        with torch.no_grad():
+            embedding_output = model.mobilebert.embeddings(
+                input_ids=batch["input_ids"],
+                token_type_ids=batch["token_type_ids"]
+            )
+        embedding_output = embedding_output.detach().numpy().astype(np.float64)
+        attention_mask = (1.0 - batch["attention_mask"]).cpu().numpy() * np.finfo(np.float32).min
+
+        folder = os.path.join(data_dir, str(step))
+        os.makedirs(folder, exist_ok=True)
+        write_fp64(os.path.join(folder, "embedding_output"), embedding_output)
+        write_fp64(os.path.join(folder, "attention_mask"), attention_mask)
+
+
+def process_glue_dataset(model, model_name_or_path, task_name, data_dir, finetuning=False):
+    raw_datasets = load_dataset("glue", task_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+
+    sentence1_key, sentence2_key = task_to_keys[task_name]
 
     def preprocess_function(examples):
-        if glue_dataset == "sst2":
-            result = tokenizer(examples["sentence"], padding="max_length", max_length=128, truncation=True)
-        else:
-            result = tokenizer(examples["sentence1"], examples["sentence2"], padding="max_length", max_length=128, truncation=True)
+        # Tokenize the texts
+        texts = (
+            (examples[sentence1_key],) if sentence2_key is None else (examples[sentence1_key], examples[sentence2_key])
+        )
+        result = tokenizer(*texts, padding="max_length", max_length=128, truncation=True)
         result["labels"] = examples["label"]
         return result
 
@@ -82,9 +185,10 @@ def write_dataset(model, glue_dataset, model_dir, data_dir, finetuning=False):
 
         for idx, batch in enumerate(tqdm(dataloader, desc=f"Running through {set_name} dataset")):
             inputs = {k: v.to(device) for k, v in batch.items()}
-            print(model(**inputs))
-            quit()
-            embedding_output = model.mobilebert.embeddings(input_ids=inputs["input_ids"], token_type_ids=inputs["token_type_ids"])
+            embedding_output = model.mobilebert.embeddings(
+                input_ids=inputs["input_ids"],
+                token_type_ids=inputs["token_type_ids"]
+            )
             embedding_output = embedding_output.detach().numpy().astype(np.float64)
             attention_mask = (1.0 - inputs["attention_mask"]).cpu().numpy() * np.finfo(np.float32).min
             labels = inputs["labels"].detach().numpy().astype(np.float64)
@@ -97,12 +201,11 @@ def write_dataset(model, glue_dataset, model_dir, data_dir, finetuning=False):
                 os.makedirs(folder)
             
             # write embedding output and attention mask
-            # write_fp64(os.path.join(folder, "embedding_output"), embedding_output)
-            # write_fp64(os.path.join(folder, "attention_mask"), attention_mask)
+            write_fp64(os.path.join(folder, "embedding_output"), embedding_output)
+            write_fp64(os.path.join(folder, "attention_mask"), attention_mask)
 
 
 def export_to_onnx(model, model_dir, output_onnx_path):
-    
     # Create dummy inputs for the model
     tokenizer = AutoTokenizer.from_pretrained(model_dir)
     dummy_sentence = "This is a sample sentence for ONNX export."
@@ -141,29 +244,30 @@ def export_to_onnx(model, model_dir, output_onnx_path):
 
 def main():
     parser = argparse.ArgumentParser(description="Generate datafiles from model.")
-    parser.add_argument("--model_dir", type=str, help="Path to model directory.", default=None, required=True)
-    parser.add_argument(
-        "--output_dir",
-        type=str,
-        default=None,
-        help="Path to output datafiles.",
-        required=True
-    )
+    parser.add_argument("--model_name_or_path", help="Path to model directory.", default=None, required=True)
+    parser.add_argument("--dataset", help="Dataset to dump.", default="sst2")
+    parser.add_argument("--output_dir", help="Path to output datafiles.", required=True)
     parser.add_argument("--dump_model", action="store_true", help="Dump model parameters.")
     parser.add_argument("--dump_dataset", action="store_true", help="Dump dataset.")
-    parser.add_argument("--dataset", type=str, help="Dataset to dump.", default="sst2")
     parser.add_argument("--finetuning", action="store_true", help="Dump dataset for finetuning.")
     args = parser.parse_args()
 
-    model = AutoModelForSequenceClassification.from_pretrained(args.model_dir, ignore_mismatched_sizes=True)
+    if args.dataset == "squad":
+        model = AutoModelForQuestionAnswering.from_pretrained(args.model_name_or_path)
+    else:
+        model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path)
 
     if args.dump_model:
         write_model_params(model, args.output_dir)
+
     if args.dump_dataset:
-        write_dataset(model, args.dataset, args.model_dir, args.output_dir, args.finetuning)
-    
-    # export_to_onnx(model, args.model_dir, "model.onnx")
-    
+        if args.dataset == "squad":
+            process_qa_dataset(model, args.model_name_or_path, args.dataset, args.output_dir)
+        else:
+            process_glue_dataset(model, args.model_name_or_path, args.dataset, args.output_dir, args.finetuning)
+
+    # export_to_onnx(model, args.model_name_or_path, "model.onnx")
+
 
 if __name__ == "__main__":
     main()
--- models/mobilebert/dump_training_data.py
+++ models/mobilebert/dump_training_data.py
@@ -2,23 +2,9 @@
 
 import argparse
 import dill as pickle
-import struct
 import os
+from utils import write_fp64
 
-import numpy as np
-
-
-''' @brief: Writes data of form torch.tensor dtype=float64 to binary data. '''
-def write_fp64(filename, data):
-    data = data.astype(np.float64)
-    with open(filename, 'wb') as f:
-        floatlist = []
-        
-        for i in np.nditer(data):
-            floatlist.append(i)
-
-        buf = struct.pack('%sd' % len(floatlist), *floatlist)
-        f.write(buf)
 
 def dump_pickled_data(path, output_dir):
     with open(path,  'rb') as f:
