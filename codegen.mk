8c1642c36e0f36509a69cb2cbd4cd7f2b7e1884f
Kartik Prabhu <kprabhu7@stanford.edu>
2024-09-11 17:47:32 -0700

    Add additional networks and datatypes
    Former-commit-id: d62b9c559fae7ccdcacaddbf6f7100e653063367

--- codegen.mk
+++ codegen.mk
@@ -1,10 +1,35 @@
 # Makefile targets for different codegen models/datatypes
 # Each target has the format: test/compiler/networks/$(NETWORK)/$(DATATYPE)/params.pb
 
+################################################################################
+# ResNet18
+################################################################################
 test/compiler/networks/resnet18/INT8/params.pb: quantized-training/test/test_codegen.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model resnet18 --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log
+	python quantized-training/test/test_codegen.py --model resnet18 --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
 
 test/compiler/networks/resnet18/E4M3/params.pb: quantized-training/test/test_codegen.py
 	mkdir -p $(dir $@)
-	python quantized-training/test/test_codegen.py --model resnet18 --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log
+	python quantized-training/test/test_codegen.py --model resnet18 --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+
+################################################################################
+# ResNet50
+################################################################################
+test/compiler/networks/resnet50/INT8/params.pb: quantized-training/test/test_codegen.py
+	mkdir -p $(dir $@)
+	python quantized-training/test/test_codegen.py --model resnet50 --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+
+test/compiler/networks/resnet50/E4M3/params.pb: quantized-training/test/test_codegen.py
+	mkdir -p $(dir $@)
+	python quantized-training/test/test_codegen.py --model resnet50 --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+
+################################################################################
+# MobileBERT
+################################################################################
+test/compiler/networks/mobilebert/INT8/params.pb: quantized-training/test/test_codegen.py
+	mkdir -p $(dir $@)
+	python quantized-training/test/test_codegen.py --model mobilebert --activation int8,qs=per_tensor_symmetric --weight int8,qs=per_tensor_symmetric --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
+
+test/compiler/networks/mobilebert/E4M3/params.pb: quantized-training/test/test_codegen.py
+	mkdir -p $(dir $@)
+	python quantized-training/test/test_codegen.py --model mobilebert --activation fp8_e4m3 --weight fp8_e4m3 --bf16 --output_dir $(dir $@) > $(dir $@)/codegen.log 2>&1
