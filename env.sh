7bb1b54a31e604b6ccd72415aa0119e4c761431d
Kartik Prabhu <kprabhu7@stanford.edu>
2024-12-02 14:30:26 -0800

    Upgrade to Catapult 2024.2_1
    Former-commit-id: 9ecf5023845c754d0a84f29e61cd14ba627c1540

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
-eval `modulecmd bash load base catapult/2024.2 vcs/T-2022.06-SP2 verdi/T-2022.06-SP2`
+eval `modulecmd bash load base catapult/2024.2_1 vcs/T-2022.06-SP2 verdi/T-2022.06-SP2`
 export VG_GNU_PACKAGE=/cad/synopsys/vcs_gnu_package/S-2021.09/gnu_9/linux
 source /cad/synopsys/vcs_gnu_package/S-2021.09/gnu_9/linux/source_me.sh
-export LD_LIBRARY_PATH=/cad/mentor/2024.2/Mgc_home/lib:/cad/mentor/2024.2/Mgc_home/shared/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/cad/mentor/2024.2_1/Mgc_home/lib:/cad/mentor/2024.2_1/Mgc_home/shared/lib:$LD_LIBRARY_PATH
 # TODO: both of these can be moved to the makefile and run_regression.sh
 export PROJECT_ROOT=`pwd`
 export CODEGEN_DIR=test/compiler
