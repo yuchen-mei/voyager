b10208a2835809632cc1030f216710d78fbd973b
Jeffrey Yu <jeffreyy@stanford.edu>
2024-12-07 10:43:25 -0800

    Add default argument for get_params.
    Former-commit-id: 897838e03774dd807f890773338bd8363dc0cd71

--- test/common/Network.h
+++ test/common/Network.h
@@ -7,7 +7,7 @@ class Network {
  public:
   Network(std::string& model);
 
-  std::vector<codegen::AcceleratorParam> get_params(bool);
+  std::vector<codegen::AcceleratorParam> get_params(bool filter_nop = true);
   std::vector<codegen::AcceleratorParam> get_params(
       const std::vector<std::string>& names, bool filter_nop = true);
 
