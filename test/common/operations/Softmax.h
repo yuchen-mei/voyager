e27b2af2efff1879cd4c489094b381a79641bb2e
Zhouhua Xie <xzh015@caddy11.stanford.edu>
2025-07-13 14:09:19 -0700

    resolve softmax issue and tanh gold model for accuracy

--- test/common/operations/Softmax.h
+++ test/common/operations/Softmax.h
@@ -25,11 +25,7 @@ inline T *softmax(std::any input_ptr, const std::vector<int> shape) {
 
     for (int j = 0; j < num_cols; j++) {
       T normalized = static_cast<T>(inputs[offset + j] - max);
-      outputs[offset + j] = poly_approx(normalized,
-        EXP_MAXES,
-        EXP_RANGES,
-        EXP_CLAMP_MIN,
-        EXP_CLAMP_MAX);
+      outputs[offset + j] = normalized.exponential();
     }
 
     // perform a tree addition
